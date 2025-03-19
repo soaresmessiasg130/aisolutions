f456858e983fa7c7ddc2fbf9fc664c23ef7219be
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2025-03-19 17:11:23 -0300

    changes(generateimageai): add the code, add README;

--- GenerateImageAI/Program.cs
+++ GenerateImageAI/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using OpenAI.Images;
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+string openAIKey = config["OpenAIKey"]!;
+string modelName = config["ModelName"]!;
+
+ImageClient imageClient = new(modelName, openAIKey);
+
+// MODIFY THIS BY YOUR PROMPT
+string prompt = """Generate a beautiful image of a otter!""";
+
+ImageGenerationOptions imageGenerationOptions = new()
+{
+  Size = GeneratedImageSize.W1024xH1024,
+};
+
+GeneratedImage generatedImage = await imageClient.GenerateImageAsync(prompt, imageGenerationOptions);
+
+Console.WriteLine($"The image are located at {generatedImage.ImageUri}");
---
+++ README.md
@@ -0,0 +1,19 @@
+# AISolutions
+
+## GeneratedImageAI
+
+Use your OpenAI API Key to generate images.
+
+### Requirements
+
+.NET 9.
+
+### Instructions to run
+
+Go to `Program.cs` file, and modify the `prompt` variable.
+
+On the Console run `dotnet run` inside `GeneratedImageAI` folder.
+
+The output will show the image file location.
+
+Enjoy!
